4354eca64ff5d6e102e1af118c98821462a670cb
learnavi <avishukla8836@gmail.com>
2025-05-01 23:12:55 +0530

    Add files via upload
     task 1 ans task 2

---
+++ task.py
@@ -0,0 +1,15 @@
+#task 1
+a=input("enter the 1 no: ")
+b=input("enter the 2 no: ")
+a=int(a)
+b=int(b)
+print("add ",a+b)
+print("sub",a-b)
+print("multipy",a*b)
+print("divide",a/b)
+
+
+#task 2
+a=input('enter your 1st name')
+b=input('enteryour last name')
+print( 'hello',a,b,'!welcome to python programme')
